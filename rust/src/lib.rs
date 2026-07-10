acf306f09c17832423ed578a304a9a054205d564
leesiuhin7 <leesiuhin7@gmail.com>
2026-07-10 11:52:14 +0100

    feat: remove unneeded code from rust template

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,17 +2,4 @@ mod data;
 mod filter;
 mod query;
 mod sorting;
-mod utils;
 mod wasm;
-
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, gge-oracle-viewer-wasm!");
-}
