ff5ec1e0145c1767d8fe348487ae9428be82a568
leesiuhin7 <leesiuhin7@gmail.com>
2026-08-01 15:19:42 +0100

    build: add base public path configuration
    - Allow base public path to be configured to allow assets to be correctly loaded in GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import wasm from "vite-plugin-wasm";
 
-export default defineConfig({
-  worker: {
-    format: "es",
-    plugins: () => [wasm()],
-  },
-  plugins: [react(), wasm()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_BASE_PATH,
+    worker: {
+      format: "es",
+      plugins: () => [wasm()],
+    },
+    plugins: [react(), wasm()],
+  };
 });
