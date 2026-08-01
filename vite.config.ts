b24114aa0c30b5d26658fc228c1cd207b858745f
leesiuhin7 <leesiuhin7@gmail.com>
2026-08-01 01:01:55 +0100

    build; fix issues when building web worker
    - Update output format to "es" to support top-level await
    - Add wasm plugin to integrate with wasm-pack

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
+  worker: {
+    format: "es",
+    plugins: () => [wasm()],
+  },
   plugins: [react(), wasm()],
 });
